619d625fd98283c26eb2865806f7a5cb34ddbdd5
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-12-29 19:31:10 +0530

    program

---
+++ Relational.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter First Number : "))#200
+num2=int(input("Enter Second Number : "))#200
+print("Less Than : ", num1<num2)#F
+print("Greater than : ",num1>num2)#F
+print("Less Than or equal to : ", num1<=num2)#T
+print("Greater than or equal to : ", num1>=num2)#T
+print("Equal to : ", num1==num2)#T
+print("Not equal to : ", num1!=num2)#F
