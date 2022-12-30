6da47853cdff19e0cb2c87cf2ffd2bbb3f4eadfa
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-12-30 19:53:27 +0530

    program

---
+++ bitwiseswap.py
@@ -0,0 +1,19 @@
+a=int(input("Enter First Number : "))#10
+b=int(input("Enter Second Number : "))#20
+'''a=a^b
+b=a^b
+a=a^b'''
+
+
+'''a=a+b#30
+b=a-b#10
+a=a-b#20'''
+
+
+a=a*b
+b=a/b
+a=a/b
+
+
+print("value of a : ", int(a))
+print("Value of b : ",int(b))
---
+++ logical.py
@@ -0,0 +1,5 @@
+num1=int(input("Enter First Number : "))#100
+num2=int(input("Enter Second Number : "))#200
+print(" Logical and : ", (num1>num2 and num1==num2))
+print("Logical Or  : ", (num1<num2 or num1!=num2))
+print("Logical Not : ", not(num1>num2))
